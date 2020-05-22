ecb3fd662e4d6754663be808f81d412975d2ef81
Musab Kılıç <musabkilic@protonmail.com>
2020-05-22 22:02:29 +0300

    removed jQuery, popperjs, p5js & bootstrapjs

--- content/js/DersBot.js
+++ content/js/DersBot.js
@@ -1,42 +1,26 @@
-function setup() {
-  noCanvas();
-
-  let bot = new RiveScript({utf8: true});
+let bot = new RiveScript({utf8: true});
   bot.unicodePunctuation = new RegExp(/[.,!?;:]/g);
-  //unused code
-  // bot.loadFile('brain.rive', brainReady, brainError);
-  // //bot.sortReplies();
   bot.loadFile("brain.rive", function() {
-  console.log("Brain loaded!");
-  bot.sortReplies();
-}, function(err, filename, lineno) {
-  console.log("An error occurred!");
-});
-
-  let username = "local-user";
-  
-  //unused code
-  // function brainReady() {
-  //   console.log('Chatbot ready!');
-  //unused code
-  // }
-  //unused code
-  // function brainError() {
-  //   console.log('Chatbot error!');
-  // }
-
-  let button = select('#submit');
-  let user_input = select('#user_input');
-  let output = select('#output');
-
-  button.mousePressed(chat);
-
-  function chat() {
+    console.log("Brain loaded!");
     bot.sortReplies();
-    let input = user_input.value();
-    let reply = bot.reply(username, input).then(function(reply) {
+  }, function(err, filename, lineno) {
+    console.log("An error occurred!");
+  });
+
+let username = "local-user";
+
+
+let button = document.getElementById("submit");
+let user_input = document.getElementById("user_input");
+let output = document.getElementById("output");
+
+button.onclick = chat;
+
+function chat() {
+  bot.sortReplies();
+  let input = user_input.value;
+  let reply = bot.reply(username, input).then(function(reply) {
     console.log("The bot says: " + reply);
-    output.html(reply);
-    });
-  }
-}
+    output.innerHTML = reply;
+  });
+};
