2849e20d24c1da222d1627327e19442ae9d1e30e
Musab Kılıç <musabkilic@protonmail.com>
2020-05-22 22:57:49 +0300

    fix all known errors

--- content/js/DersBot.js
+++ content/js/DersBot.js
@@ -1,12 +1,3 @@
-let bot = new RiveScript({utf8: true});
-  bot.unicodePunctuation = new RegExp(/[.,!?;:]/g);
-  bot.loadFile("brain.rive", function() {
-    console.log("Brain loaded!");
-    bot.sortReplies();
-  }, function(err, filename, lineno) {
-    console.log("An error occurred!");
-  });
-
 let username = "local-user";
 
 
@@ -14,13 +5,23 @@ let button = document.getElementById("submit");
 let user_input = document.getElementById("user_input");
 let output = document.getElementById("output");
 
-button.onclick = chat;
-
 function chat() {
-  bot.sortReplies();
   let input = user_input.value;
   let reply = bot.reply(username, input).then(function(reply) {
     console.log("The bot says: " + reply);
     output.innerHTML = reply;
-  });
+  }).catch(console.log);
 };
+
+
+let bot = new RiveScript({utf8: true});
+bot.unicodePunctuation = new RegExp(/[.,!?;:]/g);
+
+bot.loadFile("./brain.rive").then(function() {
+    console.log("Brain loaded!");
+    bot.sortReplies();
+    button.onclick = chat;
+  })
+  .catch(function(err, filename, lineno) {
+    console.log(err, filename, lineno);
+});
